542adfa218b66c006cf2d9122346bb75b308c594
Agustin Pivetta <agustin@telnesstech.com>
2025-01-03 11:13:45 +0100

    putting it all together

--- src/main.go
+++ src/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"time"
 
 	"github.com/cinar/indicator/v2/helper"
 	"github.com/cinar/indicator/v2/momentum"
@@ -30,16 +30,17 @@ func main() {
 	btc := bd[0]
 
 	// init indicators
-	bb := volatility.NewBollingerBands[float64]()
+	bollingerBands := volatility.NewBollingerBands[float64]()
 	emaFast := trend.NewEmaWithPeriod[float64](5)
 	emaSlow := trend.NewEmaWithPeriod[float64](20)
 	rsi := momentum.NewRsi[float64]()
 	macd := trend.NewMacd[float64]()
-	st := volatility.NewSuperTrend[float64]()
+	superTrend := volatility.NewSuperTrend[float64]()
 
+	var st, ub, mb, lb, e5, e20, r14, ml, ms float64
 	go func() {
-		superTrend := st.Compute(high[0], low[0], close[0])
-		upperBand, middleBand, lowerBand := bb.Compute(close[1])
+		superTrend := superTrend.Compute(high[0], low[0], close[0])
+		upperBand, middleBand, lowerBand := bollingerBands.Compute(close[1])
 		ema5 := emaFast.Compute(close[2])
 		ema20 := emaSlow.Compute(close[3])
 		rsi14 := rsi.Compute(close[4])
@@ -47,57 +48,63 @@ func main() {
 
 		go func() {
 			for {
-				st := <-superTrend
-				log.Printf("SuperTrend: %.2f", st)
+				st = <-superTrend
 			}
 		}()
 
 		go func() {
 			for {
-				ub := <-upperBand
-				log.Printf("UpperBand: %.2f", ub)
-				mb := <-middleBand
-				log.Printf("MiddleBand: %.2f", mb)
-				lb := <-lowerBand
-				log.Printf("LowerBand: %.2f", lb)
+				ub = <-upperBand
+				mb = <-middleBand
+				lb = <-lowerBand
 			}
 		}()
 
 		go func() {
 			for {
-				e5 := <-ema5
-				log.Printf("EMA5: %.2f", e5)
+				e5 = <-ema5
 			}
 		}()
 
 		go func() {
 			for {
-				e20 := <-ema20
-				log.Printf("EMA20: %.2f", e20)
+				e20 = <-ema20
 			}
 		}()
 
 		go func() {
 			for {
-				r14 := <-rsi14
-				log.Printf("RSI14: %.2f", r14)
+				r14 = <-rsi14
 			}
 		}()
 
 		go func() {
 			for {
-				ml := <-macdLine
-				log.Printf("MACDLine: %.2f", ml)
-				ms := <-macdSignal
-				log.Printf("MACDSignal: %.2f", ms)
+				ml = <-macdLine
+				ms = <-macdSignal
 			}
 		}()
 	}()
 
+	stabilization := 0
 	for kline := range btc.Klines {
-		fmt.Println(kline)
 		closeSource <- kline.Close
 		lowSource <- kline.Low
 		highSource <- kline.High
+
+		if stabilization < 275 {
+			stabilization++
+		} else {
+			time.Sleep(500 * time.Millisecond)
+			log.Printf("SuperTrend: %.2f", st)
+			log.Printf("UpperBand: %.2f", ub)
+			log.Printf("MiddleBand: %.2f", mb)
+			log.Printf("LowerBand: %.2f", lb)
+			log.Printf("EMA5: %.2f", e5)
+			log.Printf("EMA20: %.2f", e20)
+			log.Printf("RSI14: %.2f", r14)
+			log.Printf("MACDLine: %.2f", ml)
+			log.Printf("MACDSignal: %.2f", ms)
+		}
 	}
 }
