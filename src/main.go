3d09b82ac893f42e613281fa9b7aefb20860dcfb
Agustin Pivetta <agustin@telnesstech.com>
2025-02-18 23:27:19 +0100

    hackish fix

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -11,13 +13,76 @@ import (
 
 	"github.com/cinar/indicator/v2/helper"
 	"github.com/cinar/indicator/v2/strategy"
+	"pivetta.se/crypro-spotter/src/genetics"
 	"pivetta.se/crypro-spotter/src/ingestors"
+	"pivetta.se/crypro-spotter/src/repositories"
 	"pivetta.se/crypro-spotter/src/strategies"
 	"pivetta.se/crypro-spotter/src/utils"
 )
 
+// yes i know, global bad
 var outcome float64
 var asset *string
+
+// this is dup code, find a place to put it and consolidate
+func fetchSnapshots(db *sql.DB, symbol string, bi ingestors.BinanceIngestor) {
+	recentSnapshot, err := repositories.GetLatestSnapshot(db, symbol)
+	if err != nil {
+		log.Fatalf("Error fetching most recent snapshot: %v\n", err)
+	}
+	fmt.Printf("Most recent snapshot for %s: %+v\n", symbol, recentSnapshot)
+
+	var date time.Time
+	if recentSnapshot == nil {
+		date = time.Now().Add(-15 * 24 * time.Hour).Truncate(24 * time.Hour).UTC()
+		log.Printf("No snapshots found for %s. Fetching all snapshots since %v\n", symbol, date)
+	} else {
+		repositories.Cleanup(db, symbol)
+		date = recentSnapshot.Date
+		log.Printf("Fetching snapshots since %v\n", date)
+	}
+
+	ss := bi.GetHistory(symbol, date)
+	repositories.InsertSnapshots(db, symbol, ss)
+}
+func geneticsRun(days int, asset string) {
+	historyMinutes := 24 * 60 * days
+	repo, err := repositories.NewDBRepository(asset, historyMinutes+60)
+	if err != nil {
+		log.Fatalf("Error creating repository: %v", err)
+	}
+
+	best, err := genetics.RunGenetic(repo, asset)
+	if err != nil {
+		log.Fatalf("Error running genetic algorithm: %v", err)
+	}
+
+	log.Printf("Best strategy: %+v", best)
+	err = storeWeights(asset, best)
+	if err != nil {
+		log.Fatalf("Error storing weights: %v", err)
+	}
+}
+
+func storeWeights(a string, best *genetics.Score) error {
+	db := utils.GetDb()
+
+	jsonData, err := json.Marshal(best.Individual)
+	if err != nil {
+		return err
+	}
+	genome := string(jsonData) // Store JSON as string in DB
+
+	query := `INSERT INTO genomes (asset, date, genome, fitness) VALUES ($1, $2, $3, $4)`
+	_, err = db.Exec(query, a, time.Now(), genome, best.Value)
+	if err != nil {
+		return fmt.Errorf("storeWeights: %w", err)
+	}
+
+	return nil
+}
+
+// end of dup code
 
 func main() {
 	// Create a channel to listen for OS signals
@@ -29,6 +94,7 @@ func main() {
 		sig := <-sigChan
 		fmt.Println("\nReceived signal:", sig)
 		cleanup()
+		os.Exit(0)
 	}()
 
 	asset = flag.String("asset", "BTCUSDT", "Asset to backtest")
@@ -45,8 +111,13 @@ func main() {
 		Key:    apiKey,
 		Secret: apiSecret,
 	}
+	db := utils.GetDb()
 
-	liveRun(bi, *asset)
+	for {
+		fetchSnapshots(db, *asset, bi)
+		geneticsRun(3, *asset)
+		liveRun(bi, *asset)
+	}
 	// b, err := bi.GetBalance()
 	// if err != nil {
 	// 	log.Fatalf("Error getting balance: %v", err)
@@ -83,7 +154,7 @@ func liveRun(bi ingestors.BinanceIngestor, asset string) {
 		log.Printf("Action: %v, Price: %.2f, Outcome: %.2f", extendedAnnotation(a), kline.Close, outcome)
 
 		// we should run the compute until midnight, store the outcome and retrain the weights
-		if time.Since(startDate) >= 23*time.Hour {
+		if time.Since(startDate) >= 24*time.Hour {
 			log.Printf("End of day, retraining weights")
 			// TODO: CLOSE ALL POSITIONS when live!!
 			break
@@ -99,7 +170,6 @@ func cleanup() {
 	if err != nil {
 		log.Fatalf("Error storing outcome: %v", err)
 	}
-	os.Exit(0)
 }
 
 func extendedAnnotation(a strategy.Action) string {
